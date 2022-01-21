9da74952998a9b00e1fe18bdb5e757c9c65811cb
Tricheck-A <a.petrincik@live.de>
2022-01-21 18:34:24 +0100

    added buttons for user interaction and a div with a text childNode, which gets updated after every click

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 
-
 // Computer chooses random String from 'options' and returns it
 function computerPlay () {
     const options = ["rock", "paper", "scissors"];
     const randomOption = options[Math.floor(Math.random()*options.length)];
     return randomOption;
 }
-
+/*
 // Eingabe der Player option & konvertierung zu lowerCase
 function playerPlay () {
     return prompt("Choose Rock, Paper, or Scissors").toLowerCase();
 }
 
+
 let playerSelection = playerPlay();
 let computerSelection = computerPlay();
+*/
+
+let playerSelection;
+let computerSelection;
 
 // eine Runde des Spiels, und Siegesanzeige
 
@@ -49,7 +53,7 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-// Scoreboard
+// Calculate Score
 let playerScore = 0;
 let computerScore = 0;
 
@@ -57,56 +61,88 @@ function showScore (playerSelection, computerSelection) {
 
     // Falls Gleichstand herrscht
     if (playerSelection === computerSelection) {
-        return `You: ${playerScore} / Computer: ${computerScore}`;
+        return `You: ${playerScore} | Computer: ${computerScore}`;
     }
     // P: Rock , C: Scissors
     else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return `You: ${++playerScore} / Computer: ${computerScore}`;
+        return `You: ${++playerScore} | Computer: ${computerScore}`;
     }
     // P: Rock , C: Paper
     else if (playerSelection === "rock" && computerSelection === "paper"){
-        return `You: ${playerScore} / Computer: ${++computerScore}`;
+        return `You: ${playerScore} | Computer: ${++computerScore}`;
     }
     // P: Scissors , C: Rock
     else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return `You: ${playerScore} / Computer: ${++computerScore}`;
+        return `You: ${playerScore} | Computer: ${++computerScore}`;
     }
     // P: Scissors , C: Paper
     else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return `You: ${++playerScore} / Computer: ${computerScore}`;
+        return `You: ${++playerScore} | Computer: ${computerScore}`;
     }
     // P: Paper , C: Rock
     else if (playerSelection === "paper" && computerSelection === "rock"){
-        return `You: ${++playerScore} / Computer: ${computerScore}`;
+        return `You: ${++playerScore} | Computer: ${computerScore}`;
     }
     // P: Paper , C: Scissors
     else if (playerSelection === "paper" && computerSelection === "scissors"){
-        return `You: ${playerScore} / Computer: ${++computerScore}`;
+        return `You: ${playerScore} | Computer: ${++computerScore}`;
     }
 }
 
 
 function game(){
-    while ((playerScore && computerScore) < 5){
-        
-        if ((playerScore || computerScore ) === 5){
-            break;
-        }
-
-        else{
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(showScore(playerSelection, computerSelection));
-        playerSelection = playerPlay();
-        computerSelection = computerPlay();
-        }
-    }
 
     if (playerScore == 5){
         alert("You won the game!")
     }
     else if (computerScore == 5){
         alert("You lost the game")
-    }
-}
+    } 
+};
 
-console.log(game());
+let rockBtn = document.getElementById('rockBtn');
+let paperBtn = document.getElementById('paperBtn');
+let scissorsBtn = document.getElementById('scissorsBtn');
+
+
+// Button selections
+rockBtn.addEventListener('click', function (){
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+    playRound();
+    updateScore();
+});
+
+paperBtn.addEventListener('click', function (){
+    playerSelection = "paper";
+    computerSelection = computerPlay()
+    playRound();
+    updateScore();
+});
+
+scissorsBtn.addEventListener('click', function (){
+    playerSelection = "scissors";
+    computerSelection = computerPlay()
+    playRound();
+    updateScore();
+});
+
+
+// Scoreboard
+let scoreBoard = document.getElementById('scoreBoard');
+let scoreInfo;
+
+function updateScore(){
+
+    if (playerScore == 5){
+        scoreBoard.innerHTML = "You won the game!";
+    }
+    else if (computerScore == 5){
+        scoreBoard.innerHTML = "You lost the game";
+    }
+    else {
+    scoreInfo = playRound(playerSelection, computerSelection) + '<br>' + 
+                showScore(playerSelection, computerSelection);
+    scoreBoard.innerHTML = scoreInfo;
+    }
+};
